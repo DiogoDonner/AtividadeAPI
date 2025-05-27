bcee364d198673e6848efc8fbc5068ae9b81671a
DIOGO PATRICIO DONNER <diogo.donner@alunos.sc.senac.br>
2025-05-26 21:35:25 -0300

    Fim da aula

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ function buscarCep() {
     const valorCep = inputCep.value;
     console.log("buscando cep " + valorCep);
     fetch("https://brasilapi.com.br/api/cep/v2/" + valorCep)
-      .then((resposta) => {
-        return resposta.json();
-      })
-      .then((json) => {
-        console.log("O estado é " + json.state);
-        console.log("A cidade é " + json.city);
-        console.log("O bairro é " + json.neighborhood);
-        console.log("A rua é " + json.street);
-      });
-  }
-  
-  function configurarEventos() {
+        .then((resposta) => {
+            return resposta.json();
+        })
+        .then((json) => {
+            console.log("O estado é " + json.state);
+            console.log("A cidade é " + json.city);
+            console.log("O bairro é " + json.neighborhood);
+            console.log("A rua é " + json.street);
+        });
+
+    if (!inputCep || !valorCep) {
+        alert("Por Favor Insira um CEP")
+    }
+}
+
+function ExibirDados() {
+
+    const tabela = document.getElementById("tabela_cep").querySelector("tbody");
+
+
+}
+
+function configurarEventos() {
     const inputCep = document.getElementById("input_cep");
     inputCep.addEventListener("focusout", buscarCep);
-  }
-  
-  window.addEventListener("load", configurarEventos);
+    console.log(transformedData);
+}
+
+window.addEventListener("load", configurarEventos);
